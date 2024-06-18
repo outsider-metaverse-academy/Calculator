d7e3ccb7977a0aa1d64e0cefba5e52ec6a5ab593
daekwon2000 <daekklove@gmail.com>
2024-06-18 16:23:33 +0900

    더하기 기능 개발 완료 (#1)
    상세내용
    두 수를 더한 결과를 리턴하는 함수를 테스트코드와 함께 개발 완료함

--- src/main/java/com/outsider/Calculator/Adder.java
+++ src/main/java/com/outsider/Calculator/Adder.java
@@ -1,4 +1,10 @@
 package com.outsider.Calculator;
 
+import java.util.Scanner;
+
 public class Adder {
+    public int add (int first, int second) {
+        int result = first + second;
+        return result;
+    }
 }
